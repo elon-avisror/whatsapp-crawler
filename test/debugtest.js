f4982fb3a7c9eb8fe7061558a857c0cbfe26cf10
elonavisrur <elavis2@gmail.com>
2019-04-11 13:05:28 +0300

    cleaning directories

--- test/debugtest.js
+++
@@ -1,5 +0,0 @@
-const mod = require("../analysis/server/api/analysis.js");
-
-//mod.getMessages({ts:00000});
-var a = mod.getTablesData();
-var s = 0;
--- unidress_crawler/continue/classes/OurRegExp.js
+++
@@ -1,6 +0,0 @@
-class OurRegExp extends RegExp {
-  [Symbol.split](str, limit) {
-    var result = RegExp.prototype[Symbol.split].call(this, str, limit);
-    return result.map(x => "[" + x + "]");
-  }
-}
