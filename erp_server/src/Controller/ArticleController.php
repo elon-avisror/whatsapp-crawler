74dedca4424d7096304df6ff39e3c3808b725115
elonavisrur <elavis2@gmail.com>
2019-04-30 17:16:17 +0300

    configure erp_server

--- erp_server/src/Controller/ArticleController.php
+++ erp_server/src/Controller/ArticleController.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: אייל נעמן
- * Date: 25/10/2018
- * Time: 09:32
- */
 
 namespace App\Controller;
-
 
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -16,14 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController
 {
 
-
-
-
-
     /**
      * @Route("/")
      */
-    public function homepage(LoggerInterface $logger){
+    public function homepage(LoggerInterface $logger)
+    {
         $logger->info("Hello");
         $logger->critical("critic");
         return new Response('my first page122242');
@@ -32,9 +22,8 @@ class ArticleController
     /**
      * @Route("/news/{data1}/{data2}")
      */
-    public function show($data1,$data2){
-       return new Response($data1 .'<->'. $data2);
+    public function show($data1, $data2)
+    {
+        return new Response($data1 . '<->' . $data2);
     }
-
-
 }
