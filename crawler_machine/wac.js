17d048ba7a6b674b790ec6be9cea50a9a833a156
elonavisrur <aelon@cambium.co.il>
2019-05-31 17:24:13 +0300

    a little fix in zone

--- crawler_machine/wac.js
+++ crawler_machine/wac.js
@@ -167,7 +167,7 @@ async function loginCheck() {
       await getValidationLinks(counterForSend);
 
       date = new Date();
-      let zone = date.getTimezoneOffset();
+      zone = date.getTimezoneOffset();
       date.setHours((date.getHours()+(-1)*zone)%24);
 
       console.log("now " + date.toString() + "!\ni'm going to sleep for 10 minutes...");
